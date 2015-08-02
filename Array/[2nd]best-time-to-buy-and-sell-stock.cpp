845665f2ad6ddbbf2185b68c8ef20305c6fa4e61
Honam Bhang <walkingtospace@gmail.com>
2015-08-02 09:55:48 -0700

    Update and rename best-time-to-buy-and-sell-stock.cpp to [2nd]best-time-to-buy-and-sell-stock.cpp

--- Array/[2nd]best-time-to-buy-and-sell-stock.cpp
+++ Array/[2nd]best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,18 @@
 /*
-O(n) : 
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
-insights 
+Question
+Say you have an array for which the ith element is the price of a given stock on day i.
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+O(n) solution
+
+insights
 1: To make profit, we should sell the stock when the price becomes 'higher'
 2: We should keep 'min' -> but the the selling price next to min wouldn't guarrante the max gap..-> store min and gap at the same time.
 3: So, compare gap with the current min.
 
-edge cases: 
+edge cases 
 1. what if all prices are ascending order?
 2. what if all prices are descending order?
 3. what if all prices are all flat? -> what should be the answer?
