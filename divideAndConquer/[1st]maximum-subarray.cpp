4afb58ba472c58ab7a4f611a53eb3c780a39dcf7
Honam Bhang <walkingtospace@gmail.com>
2015-08-05 22:19:13 -0700

    Create [1st]maximum-subarray.cpp

---
+++ divideAndConquer/[1st]maximum-subarray.cpp
@@ -0,0 +1,31 @@
+/*
+https://leetcode.com/problems/maximum-subarray/
+
+Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+
+*/
+
+public class Solution {
+    public int maxSubArray(int[] A) {
+        int max = A[0];
+        int sum = A[0];
+        
+        for(int i=1; i < A.length; i++) {
+            sum = sum + A[i];
+            if(sum > max) {
+                max = sum;
+            } else if(sum < A[i]) {
+                sum = A[i];
+            }
+            if(A[i] > max) {
+                sum = A[i];
+                max = A[i];
+            }
+        }
+        
+        return max;
+    }
+}
